baab0ffb9cf580d41429a8d5a91372b078d443d4
Jonathan Zajfen <jonathan.zajfen@gmail.com>
2020-07-20 01:36:42 +0300

    first commit

---
+++ makor-app/src/components/Rates.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+class RatesComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      rates: [],
+    };
+  }
+
+  componentDidMount() {
+    try {
+      setInterval(async () => {
+        var res = await fetch(
+          "https://www.live-rates.com/rates?key=a86be87a24"
+        );
+        var rateJson = await res.json();
+        this.setState({
+          isLoaded: true,
+          rates: rateJson,
+        });
+      }, 1000);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    const { error, isLoaded, rates } = this.state;
+    if (error) {
+      return <div>Error</div>;
+    } else if (!isLoaded) {
+      return <div className="loadingPage">Loading...</div>;
+    } else {
+      return (
+        <>
+          <h2 className="title">Rates</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Currency</th>
+                <th>Rate</th>
+                <th>Bid</th>
+                <th>Ask</th>
+                <th>High</th>
+                <th>Low</th>
+                <th>Open</th>
+                <th>Close</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rates.map((data) => (
+                <tr key={data.currency}>
+                  <td>{data.currency} </td>
+                  <td>{data.rate}</td>
+                  <td>{data.ask}</td>
+                  <td>{data.bid}</td>
+                  <td>{data.high}</td>
+                  <td>{data.low}</td>
+                  <td>{data.open}</td>
+                  <td> {data.close}</td>
+                  <td>
+                    {new Intl.DateTimeFormat("en-GB", {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      second: "2-digit",
+                    }).format(data.timestamp)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      );
+    }
+  }
+}
+
+export default RatesComponent;
---
+++ makor-app/src/index.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom";
+import RatesComponent from "./components/Rates";
+import "./index.css";
+
+render(<RatesComponent />, document.getElementById("root"));
